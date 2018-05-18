d180f7cb88b47fb9586a227df2428f8e59e7e90d
reinisermansons <reinisermansons@gmail.com>
2018-05-19 02:05:10 +0300

    Coin counter

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+     public Rect labelPosition;
+    string labelText;
+    public GUIStyle labelStyle;
+    public int points = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 500, 20), "Savāktās monētas : " + points);
+    }
+}
